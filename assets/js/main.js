74c83149694a51b59b89c95f943fc9b56238a435
Josh Comolli <josh@comocreative.com>
2023-11-08 21:52:13 -0500

    update 2
    update 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,44 +3,30 @@ jQuery.noConflict();
 (function($) { 
 	"use strict";
 	
-	// Circle Click Function
+	// Circle click function
 	$('#circles').on('click', '.shape', function(e) {
 		e.preventDefault();
 		
-		// Get Circle Color and Target Square 
-		var $this, shapeColor, squareTarget;
+		// Get circle element and color
+		var $this, element;
 		$this = $(this);
-		shapeColor = $this.parents('.contain').data('color');
-		squareTarget = $('#squares').children('.contain.'+ shapeColor); 
+		element = $this.parents('.contain');
 		
-		// Add active class to Target Square
-		$(squareTarget).addClass('active');
-
+		// Move Element to #squares row   
+		$(element).appendTo('#squares');
 	});
 	
 	// Square Click Function
 	$('#squares').on('click', '.shape', function(e) {
 		e.preventDefault();
 		
-		// Get Square element and Color
-		var $this, element, elemColor;
+		// Get circle element and color
+		var $this, element;
 		$this = $(this);
 		element = $this.parents('.contain');
-		elemColor = $(element).data('color');
 		
-		// Remove Circle class form any Square elements, then add Circle class/remove Square class from target element  
-		$('#squares').children('.contain .shape').not($this).removeClass('circle');
-		$(this).addClass('circle').removeClass('square');
-		
-		// With for the square to circle animation to finish, then move the target element to the #circles div 
-		setTimeout(function() { 			
-			$(element).prependTo('#circles').removeClass('active');
-			$('#circles').find('.contain[data-color="'+ elemColor +'"]').not(element).remove(); // Remove the old Circle element from ther #circles div 
-			
-			// Clone the target element to the beginning of the #squares div and turn it back into an inactive square
-			$(element).clone().prependTo('#squares');
-			$('#squares').children('.contain.'+ elemColor).removeClass('active').children('.shape').removeClass('circle').addClass('square');
-		}, 600);
+		// Move Element to #squares row   
+		$(element).prependTo('#circles');
 
 	});
 })(jQuery);
